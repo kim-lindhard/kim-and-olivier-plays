749f8971f6045e54fc6d9cb96eb55cce8b6d7677
olivier <olivier.costa@aegisoft.be>
2020-02-07 11:06:13 +0100

    Setup solution

---
+++ StockExchangeBroker/StockExchangeBroker.features/Scenario_ViewCurrentPortfolio.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace StockExchangeBroker.features
+{
+    public class Scenario_ViewCurrentPortfolio
+    {
+        [Fact]
+        public void Recipe_ViewCurrentPortfolio()
+        {
+            
+        }
+    }
+}
